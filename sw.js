c83b20361741a49dcff0a88e2e4b3e025c86c8f0
jpaps79-coder <jpaps79@gmail.com>
2026-08-20 08:20:11 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lenormand-v22";
+const CACHE = "lenormand-v25";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("message", (e) => {
